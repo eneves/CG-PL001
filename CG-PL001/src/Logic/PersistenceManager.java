b94f2681bbc799e6a4dcec7bab3659f62a41ae6c
eneves <neves.emanuel@gmail.com>
2013-11-02 13:17:29 +0000

    Merge origin/master
    Conflicts:
    	SD1314pl001/src/client/Mensage.java

---
+++ CG-PL001/src/Logic/PersistenceManager.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ *
+ * @author Emanuel
+ */
+public class PersistenceManager {
+
+    private static String filename = "highway_data";
+
+    public static Simulator loadSimulator(String filename) {
+        return null;
+    }
+
+    //Caso o ficheiro não exista, deve ser criada uma rede de estradas com as características apresentadas na tabela 1.
+    //    comprimento da estrada 10
+    //    posições das fontes 1; 4; 5
+    //    período de emissão de automóveis nas fontes 2; 4; 5
+    //    estrada direita
+    //Tabela 1: Características da rede de estradas quando é omitido o nome do ficheiro.
+    private static Simulator loadDefaults() {
+        return null;
+    }
+
+    public static void saveSimulator() {
+
+    }
+
+}
---
+++ CG-PL001/src/Logic/TrafficManager.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ *
+ * @author Emanuel
+ */
+public class TrafficManager {
+
+    public void processTraffic() {
+
+    }
+
+    public void processSources() {
+
+    }
+
+}
